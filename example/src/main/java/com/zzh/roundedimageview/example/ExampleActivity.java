844dcb28e63d1e53c4d69ba0a1993457f2d292df
墨宇 <zzh_hz@126.com>
2020-02-04 19:39:59 +0800

    圆形图片

---
+++ example/src/main/java/com/zzh/roundedimageview/example/ExampleActivity.java
@@ -0,0 +1,72 @@
+package com.zzh.roundedimageview.example;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class ExampleActivity extends AppCompatActivity
+    implements AdapterView.OnItemSelectedListener {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.example_activity);
+
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    Spinner navSpinner = (Spinner) findViewById(R.id.spinner_nav);
+
+    navSpinner.setAdapter(ArrayAdapter.createFromResource(
+        navSpinner.getContext(),
+        R.array.action_list,
+        android.R.layout.simple_spinner_dropdown_item));
+
+    navSpinner.setOnItemSelectedListener(this);
+
+    if (savedInstanceState == null) {
+      navSpinner.setSelection(0);
+    }
+  }
+
+  @Override public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    Fragment newFragment;
+    switch (position) {
+      default:
+      case 0:
+        // bitmap
+        newFragment = RoundedFragment.getInstance(RoundedFragment.ExampleType.DEFAULT);
+        break;
+      case 1:
+        // oval
+        newFragment = RoundedFragment.getInstance(RoundedFragment.ExampleType.OVAL);
+        break;
+      case 2:
+        // select
+        newFragment = RoundedFragment.getInstance(RoundedFragment.ExampleType.SELECT_CORNERS);
+        break;
+      case 3:
+        // picasso
+        newFragment = new PicassoFragment();
+        break;
+      case 4:
+        // color
+        newFragment = new ColorFragment();
+        break;
+      case 5:
+        // background
+        newFragment = RoundedFragment.getInstance(RoundedFragment.ExampleType.BACKGROUND);
+        break;
+    }
+
+    getSupportFragmentManager().beginTransaction()
+        .replace(R.id.fragment_container, newFragment)
+        .commit();
+  }
+
+  @Override public void onNothingSelected(AdapterView<?> parent) { }
+}
---
+++ example/src/main/java/com/zzh/roundedimageview/example/PicassoFragment.java
@@ -0,0 +1,102 @@
+package com.zzh.roundedimageview.example;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+import com.zzh.roundedimageview.RoundedTransformationBuilder;
+
+public class PicassoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_rounded, container, false);
+
+        PicassoAdapter adapter = new PicassoAdapter(getActivity());
+        ((ListView) view.findViewById(R.id.main_list)).setAdapter(adapter);
+
+        adapter.add(new PicassoItem("http://img5.imgtn.bdimg.com/it/u=639334165,1649889958&fm=11&gp=0.jpg", ScaleType.CENTER));
+        adapter.add(new PicassoItem("http://img5.imgtn.bdimg.com/it/u=3373541372,149267451&fm=26&gp=0.jpg", ScaleType.CENTER_CROP));
+        adapter.add(new PicassoItem("http://img2.imgtn.bdimg.com/it/u=2605828104,2415825840&fm=26&gp=0.jpg", ScaleType.CENTER_INSIDE));
+        adapter.add(new PicassoItem("http://img4.imgtn.bdimg.com/it/u=171411223,1247580741&fm=26&gp=0.jpg", ScaleType.FIT_CENTER));
+        adapter.add(new PicassoItem("http://img4.imgtn.bdimg.com/it/u=171411223,1247580741&fm=26&gp=0.jpg", ScaleType.FIT_END));
+        adapter.add(new PicassoItem("http://img4.imgtn.bdimg.com/it/u=171411223,1247580741&fm=26&gp=0.jpg", ScaleType.FIT_START));
+        adapter.add(new PicassoItem("http://img4.imgtn.bdimg.com/it/u=171411223,1247580741&fm=26&gp=0.jpg", ScaleType.FIT_XY));
+
+        return view;
+    }
+
+    static class PicassoItem {
+        final String mUrl;
+
+        final ScaleType mScaleType;
+
+        PicassoItem(String url, ScaleType scaleType) {
+            this.mUrl = url;
+            mScaleType = scaleType;
+        }
+
+    }
+
+    class PicassoAdapter extends ArrayAdapter<PicassoItem> {
+        private final LayoutInflater mInflater;
+        private final Transformation mTransformation;
+
+        public PicassoAdapter(Context context) {
+            super(context, 0);
+            mInflater = LayoutInflater.from(getContext());
+            mTransformation = new RoundedTransformationBuilder()
+                    .cornerRadiusDp(30)
+                    .borderColor(Color.BLACK)
+                    .borderWidthDp(3)
+                    .oval(false)
+                    .build();
+        }
+
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+            ViewGroup view;
+            if (convertView == null) {
+                view = (ViewGroup) mInflater.inflate(R.layout.picasso_item, parent, false);
+            } else {
+                view = (ViewGroup) convertView;
+            }
+
+            PicassoItem item = getItem(position);
+
+            ImageView imageView = ((ImageView) view.findViewById(R.id.imageView1));
+            imageView.setScaleType(item.mScaleType);
+
+
+            Transformation transformation = new RoundedTransformationBuilder()
+                    .borderColor(Color.BLACK)
+                    .borderWidthDp(3)
+                    .cornerRadiusDp(30)
+                    .oval(false)
+                    .build();
+            Picasso.with(getContext())
+                    .load(item.mUrl)
+                    .fit()
+                    .transform(transformation)
+                    .into(imageView);
+
+
+            ((TextView) view.findViewById(R.id.textView3)).setText(item.mScaleType.toString());
+            return view;
+        }
+    }
+}
